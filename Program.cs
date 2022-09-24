46408cf9df8bad4dce3d3a0edcaa69b48baad23c
Leozllin <zullisun@gmail.com>
2022-09-23 21:20:58 -0300

    independencia

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("--- Vivendo de dividendos ---\n");
+
+Console.Write("Gastos mensais (em R$)....:");
+
+string gastosMensais = Decimal Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%)...: ");
+string dividendYield = Decimal Convert.ToDouble(Console.ReadLine());
+
+string valorAInvestir = gastosMensais / Convert.ToDecimal(dividendYield 100);
+
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos de {dividendYield:N1}% você precisa ter investido {valorAInvestir:C}.");
